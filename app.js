63a9b75fdcfa73b4e90502e5b95b69938b45f221
Hhuxx1 <142674015+Hhuxx1-1@users.noreply.github.com>
2024-12-10 18:41:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const endpoint = "https://script.google.com/macros/s/AKfycbwSkZ2G1OAJ_PIPg9-4RpRaqPZ1IxACRK_w234KqsBstWxSqPW2KsL-M3--2ZZwhLcw/exec";
 
 fetch(endpoint, 
-    { method: 'POST', headers: { 'Content-Type': 'application/json' }, 
-    body: JSON.stringify({ name: 'John Doe', email: 'john.doe@example.com' }) }) 
-    .then(response => response.json()) 
-    .then(data => console.log(data)) 
-    .catch(error => console.error('Error:', error)
-);
+    { 
+        method: 'POST', 
+        mode: 'no-cors', 
+        headers: { 'Content-Type':'text/plain'},  
+        body: 'name=JohnDoe&email=john.doe@example.com',
+        redirect: 'follow'}
+    ) .then(response => console.log('Request complete')) .catch(error => console.error('Error:', error));
